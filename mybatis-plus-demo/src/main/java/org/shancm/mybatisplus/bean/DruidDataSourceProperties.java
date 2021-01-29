358cd79e8eca04d2a56cd316b5a7c8e552e154a2
cdprism <ruan-shan@163.com>
2021-01-29 14:09:48 +0800

    init

---
+++ mybatis-plus-demo/src/main/java/org/shancm/mybatisplus/bean/DruidDataSourceProperties.java
@@ -0,0 +1,30 @@
+package org.shancm.mybatisplus.bean;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "spring.datasource.druid")
+@Data
+public class DruidDataSourceProperties {
+
+    // jdbc
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+    // jdbc connection pool
+    private int initialSize;
+    private int minIdle;
+    private int maxActive = 100;
+    private long maxWait;
+    private long timeBetweenEvictionRunsMillis;
+    private long minEvictableIdleTimeMillis;
+    private String validationQuery;
+    private boolean testWhileIdle;
+    private boolean testOnBorrow;
+    private boolean testOnReturn;
+    private boolean poolPreparedStatements;
+    private int maxPoolPreparedStatementPerConnectionSize;
+    // filter
+    private String filters;
+}
