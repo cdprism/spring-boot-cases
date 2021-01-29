42ebdcc0dd60932368c7c8ada7314dce456bcb26
cdprism <ruan-shan@163.com>
2021-01-29 14:07:28 +0800

    executable

--- slf4j-demo/src/main/java/org/shancm/slf4j/Slf4jDemoApplication.java
+++ slf4j-demo/src/main/java/org/shancm/slf4j/Slf4jDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Slf4jDemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Slf4jDemoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Slf4jDemoApplication.class, args);
+	}
 
 }
--- slf4j-demo/src/main/java/org/shancm/slf4j/webapp/controller/Slf4jController.java
+++ slf4j-demo/src/main/java/org/shancm/slf4j/webapp/controller/Slf4jController.java
@@ -2,21 +2,34 @@ package org.shancm.slf4j.webapp.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 
 @RestController
 public class Slf4jController {
 
     protected final Logger logger = LoggerFactory.getLogger(getClass());
 
+//    @Value("${log4j.appender.info.File}")
+//    private String filePath;
+
+//    @Value("${profile.log4j.appender.info.File}")
+//    private String path;
+
+//    @Value("${define}")
+//    private String value;
+
     @GetMapping("/record/{message}")
     public void record(@PathVariable("message") String message) {
         logger.info(message);
         logger.error(message);
+//        System.out.println(filePath);
+//        System.out.println(path);
+//        System.out.println(value);
+
     }
 
 
